83c843c6848075c0efebc265a43803379634ed99
Vsmasster <krasnal1995@gmail.com>
2016-08-16 19:28:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ksychoo/killingmonsters/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ksychoo.killingmonsters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
